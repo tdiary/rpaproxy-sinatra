2dea98cfed7c02939cadcfbb9130edf9210372fd
MATSUOKA Kohei <kohei@machu.jp>
2011-09-21 12:28:04 +0900

    implemented authentication with twitter

--- rpaproxy.rb
+++ rpaproxy.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'mongoid'
+require 'omniauth'
 
 class Proxy
 	include Mongoid::Document
@@ -10,7 +11,41 @@ class Proxy
 	field :failure, type: Integer, default: 0
 end
 
+class User
+	include Mongoid::Document
+	field :uid, type: String
+	field :name, type: String
+	field :screen_name, type: String
+
+	def self.find_or_create_with_omniauth(auth)
+		user = where(uid: auth['uid']).first
+		unless user
+			user = create! do |user|
+				user.uid = auth['uid']
+				user.name = auth['user_info']['name']
+				user.screen_name = auth['user_info']['nickname']
+			end
+		end
+		user
+	end
+end
+
+helpers do
+	def current_user
+		@current_user ||= User.where(uid: session[:user_id]) if session[:user_id]
+	end
+end
+
+# TODO: use secure session
+enable :sessions, :logging
+#use Rack::Session::Cookie
+use OmniAuth::Builder do
+	provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+end
+
 configure do
+	raise StandardError.new("not found ENV['TWITTER_KEY']") unless ENV['TWITTER_KEY']
+	raise StandardError.new("not found ENV['TWITTER_SECRET']") unless ENV['TWITTER_SECRET']
 	Mongoid.configure do |config|
 		raise StandardError.new("not found ENV['MONGOHQ_URL']") unless ENV['MONGOHQ_URL']
 		uri  = URI.parse(ENV['MONGOHQ_URL'])
@@ -20,8 +55,19 @@ configure do
 	# Proxy.create(endpoint: "http://www.machu.jp/amazon_proxy", name: "machu", locales: ["jp", "en"])
 end
 
+get '/login' do
+	"<a href='/auth/twitter'>Sign in with Twitter</a>"
+end
+
 get '/' do
-	"Hello World!"
+	raise StandardError.new("debug")
+end
+
+get '/auth/:name/callback' do
+	auth = request.env['omniauth.auth']
+	@current_user = User.find_or_create_with_omniauth(auth)
+	session[:user_id] = @current_user.uid
+	"hello #{current_user.name}"
 end
 
 # リバースプロキシ http://rpaproxy.heroku.org/rpaproxy/jp/
