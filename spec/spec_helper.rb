11d547855a530acd756ee74d6c2ecce59bb209c6
MATSUOKA Kohei <kohei@machu.jp>
2011-09-26 00:23:49 +0900

    disabled spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 
-ENV['RACK_ENV'] = "test"
+#ENV['RACK_ENV'] = "test"
 # ENV['MONGOHQ_URL'] = 'mongodb://travis:travis@127.0.0.1:27017/myapp'
-ENV['TWITTER_KEY'] = 'your twitter key'
-ENV['TWITTER_SECRET'] = 'your twitter secret'
+#ENV['TWITTER_KEY'] = 'your twitter key'
+#ENV['TWITTER_SECRET'] = 'your twitter secret'
 
