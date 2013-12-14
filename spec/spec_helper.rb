48d0759b33eb6d194088add3df1130d86791ac0e
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-12-14 09:39:35 +0900

    cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 #ENV['RACK_ENV'] = "test"
-# ENV['MONGOHQ_URL'] = 'mongodb://travis:travis@127.0.0.1:27017/myapp'
+#ENV['MONGOHQ_URL'] = 'mongodb://travis:travis@127.0.0.1:27017/myapp'
 #ENV['TWITTER_KEY'] = 'your twitter key'
 #ENV['TWITTER_SECRET'] = 'your twitter secret'
-
