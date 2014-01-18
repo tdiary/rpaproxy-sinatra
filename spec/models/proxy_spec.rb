75b35bc42df451968da0645f659ec52ef0285b07
MATSUOKA Kohei <kohei@machu.jp>
2014-01-18 20:43:38 +0900

    add spec

--- spec/models/proxy_spec.rb
+++ spec/models/proxy_spec.rb
@@ -5,13 +5,18 @@ ILLIGAL_PROXY_ENDPOINT = 'http://illigal.example.com/endpoint/'
 
 describe Proxy do
 	before do
-		stub_request(:get, "#{PROXY_ENDPOINT}jp/?test=aaa")
-			.to_return(status: 302, :headers => { 'Location' => 'http://www.example.com' })
 		@proxy = Proxy.new(endpoint: PROXY_ENDPOINT)
 		@illigal_proxy = Proxy.new(endpoint: ILLIGAL_PROXY_ENDPOINT)
 	end
 
 	describe '#fetch' do
+		before do
+			stub_request(:get, "#{PROXY_ENDPOINT}jp/?test=aaa")
+				.to_return(status: 302, headers: { Location: 'http://www.example.com' })
+			stub_request(:get, "#{ILLIGAL_PROXY_ENDPOINT}jp/?test=aaa")
+				.to_return(status: 200)
+		end
+
 		subject { @proxy.fetch('jp', 'test=aaa') }
 
 		it 'should fetch proxy' do
@@ -20,11 +25,26 @@ describe Proxy do
 		end
 
 		context 'endpointが302以外のステータスコードを返す場合' do
-			it '例外を返すこと' do
+			it 'expect raise StandardError' do
 				expect {
 					@illigal_proxy.fetch('jp', 'test=aaa')
-				}.to raise_error
+				}.to raise_error(StandardError)
 			end
+		end
+	end
+
+	describe '#parse_yaml' do
+		before do
+			stub_request(:get, "#{PROXY_ENDPOINT}rpaproxy.yaml")
+				.to_return(status: 200, body: File.new('spec/fixtures/rpaproxy.yaml'))
+			@proxy.parse_yaml
+		end
+
+		subject { @proxy }
+
+		it 'should set the name and locales from a YAML file on the endpoint' do
+			expect(subject.name).to eq 'Amazon認証プロキシ'
+			expect(subject.locales).to eq ["jp", "us", "ca", "de", "fr", "uk"]
 		end
 	end
 end
